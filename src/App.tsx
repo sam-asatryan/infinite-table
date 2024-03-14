399f710b03cc5036746f487ee31bb32d5d20a9c1
Samvel Asatryan <samjan1995@gmail.com>
2024-03-14 20:59:09 +0400

    setups the react project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Select from "./components/select";
+import './app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Select/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/select/Select.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './select.scss'
+
+const Select = () => {
+  return (
+    <div>
+      Select
+    </div>
+  );
+};
+
+export default Select;
